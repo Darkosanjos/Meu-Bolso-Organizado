7c61776b0e6ee8797d11631bbb286f521e4daca5
Darkosanjos <124837617+Darkosanjos@users.noreply.github.com>
2024-03-17 01:03:22 -0300

    Adicionando banco de dados + tabelas

--- BCD.py
+++ BCD.py
@@ -1,1 +1,21 @@
-import sqlite3 as sql
+import sqlite3 as lite
+
+con = lite.connect('dados.db')
+
+#tabela de categoria
+with con:
+    cur=con.cursor()
+    cur.execute("CREATE TABLE Categoria(id INTEGER PRIMARY KEY AUTOINCREMENT, nome TEXT)")
+
+
+#tabela de receitas
+with con:
+    cur=con.cursor()
+    cur.execute("CREATE TABLE Receitas(id INTEGER PRIMARY KEY AUTOINCREMENT, categoria TEXT, adicionado_em DATE, Valor DECIMAL)")
+
+
+#tabela de gastos
+with con:
+    cur=con.cursor()
+    cur.execute("CREATE TABLE Gastos(id INTEGER PRIMARY KEY AUTOINCREMENT, categoria TEXT, retirado_em DATE, Valor DECIMAL)")
+
---
+++ view.py
@@ -0,0 +1,57 @@
+import sqlite3 as lite
+
+
+con = lite.connect('dados.db')
+
+#inserir ------------------------------------------------------------
+#inserir categoria
+def inserir_categoria(i):
+    with con:
+        cur = con.cursor()
+        query = "INSERT INTO Categoria (nome) VALUES (?)"
+        cur.execute(query,i)
+
+#inserir receitas
+def inserir_categoria(i):
+    with con:
+        cur = con.cursor()
+        query = "INSERT INTO Receitas (categoria, adicionando_em,valor) VALUES (?,?,?)"
+        cur.execute(query,i)
+
+#inserir gastos
+def inserir_categoria(i):
+    with con:
+        cur = con.cursor()
+        query = "INSERT INTO Gastos (categoria, retirado_em,valor) VALUES (?,?,?)"
+        cur.execute(query,i)
+
+
+#Deletes --------------------------------------------------------------
+
+def deletar_receitas(i):  
+    with con:
+        cur = con.cursor()
+        query = "DELETE FROM Receitas WHERE id=?"
+        cur.execute(query, i)
+
+def deletar_Gastos(i):  
+    with con:
+        cur = con.cursor()
+        query = "DELETE FROM Gastos WHERE id=?"
+        cur.execute(query, i)        
+
+#ver dados
+#categoria
+
+def ver_categoria():
+    lista_itens = []
+
+    with con:
+        cur = con.cursor()
+        cur.execute("SELECT * FROM Categoria")
+        linha = cur.fetchall()
+        for l in linha:
+            lista_itens.append(l)
+
+    return lista_itens
+print(ver_categoria())
