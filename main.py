91b98c2de3cb2667afc5c39271c86185d3d4b94a
Darkosanjos <brunoanjos121@gmail.com>
2024-04-22 17:53:50 -0300

    Adicionado icones futuros para o gestor

--- main.py
+++ main.py
@@ -1,14 +1,17 @@
-#tkinter
 from _tkinter import *
-from tkinter import Tk, ttk, LEFT, NW, RAISED
+from tkinter import Tk, ttk, LEFT, NW, RAISED, Label
 import tkinter as tk
 from tkinter import NSEW
-from tkinter import Label
-
 
 #pillow
 from PIL import Image, ImageTk
+#import barra de progresso do tkinter
+from tkinter.ttk import Progressbar
 
+#importando Matplotlib
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
+import matplotlib.pyplot as plt
+from matplotlib.figure import Figure
 
 ################# cores ###############
 
@@ -45,18 +48,38 @@ frameMeio = tk.Frame(janela, width=1043, height=361, bg=co1, pady=20, relief='ra
 frameMeio.grid(row=1,column=0, pady=1, padx=0, sticky=NSEW)
 
 frameBaixo = tk.Frame(janela, width=1043, height=300, bg=co1, relief='flat')
-frameBaixo.grid(row=1,column=0, pady=0, padx=10, sticky=NSEW)
+frameBaixo.grid(row=2,column=1, pady=1, padx=10, sticky=NSEW)
 
 
 # Trabalhando no framde Cima
 
 #abrindo a imagem
-app_img = Image.open('Log.png')
+app_img = Image.open('Logo.png')
 app_img = app_img.resize((45,45))
 app_img = ImageTk.PhotoImage(app_img)
 
-app_Logo = Label(frameCima, image=app_img, text="  Planejamento Pessoal", width=900, compound=LEFT, padx=5, relief=RAISED, anchor=NW, font=('Verdana 20 bold'), bg=co1, fg=co4)
+app_Logo = Label(frameCima, image=app_img, text="   Gestor de gastos ", width=900, compound=LEFT, padx=5, relief=RAISED, anchor=NW, font=('Verdana 20 bold'), bg=co1, fg=co4)
 app_Logo.place(x=0, y=0)
 
 
+# Percentagem------------------------------
+def percentagem():
+    l_nome = Label(frameMeio, text="Porcentagem da Receita gasta", height=1, anchor=NW, font=('Verdana 12'), bg=co1, fg=co4)
+    l_nome.place(x=7, y=5)
+
+    style = ttk.Style()
+    style.theme_use('default')
+    style.configure("black.Horizontal.TProgressbar", background='#daed6b')
+    style.configure("TProgressbar", thickness=25)
+
+    bar = Progressbar(frameMeio, length=180)    
+    bar.place(x=10, y=35)
+    bar['value'] = 50
+
+    valor = 50
+
+    l_percentagem = Label(frameMeio, text="{:,.2f}%".format(valor), anchor=NW, font=('Verdana 12'), bg=co1, fg=co4)
+    l_percentagem.place(x=200, y=35)
+
+percentagem()
 janela.mainloop()
